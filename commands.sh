61233b49acc1098d437c84721d8ca3b6dd5cf43b
cristhianpoveda <cristhianpoveda12@gmail.com>
2026-06-03 21:43:00 +0100

    open android studio

--- commands.sh
+++ commands.sh
@@ -5,3 +5,7 @@ ros2 launch wildbridge_nav_tests navigation.launch.py
 ros2 bag record -o open_loop_test_cube /drone_1/cartesian_position
 
 ros2 topic pub /drone_1/trigger_shape std_msgs/String "{data: 'cube'}" -1
+
+# open android studio
+
+/opt/android-studio/bin/studio.sh
